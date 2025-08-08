44ee2248a94b215e7aef32060bcf6e58017b7092
seowfie <144519653+seowfie@users.noreply.github.com>
2025-08-08 12:29:28 +0800

    Update server_routing.js

--- MIDTERMS/server_routing.js
+++ MIDTERMS/server_routing.js
@@ -25,9 +25,16 @@ app.get("/ab*cd", function(req, res) {
     res.send("Pattern Match Page");
 })
 
+// This responds a GET request for the /seowfie page
+app.get("/seowfie", function(req, res) {
+    console.log("Got a GET request for /seowfie");
+    res.send("Get to know me!<br><br>Name: Sofia Anne T. Sarmiento<br>Github User: seowfie<br>Section: WD-301<br>Course:BS Information Technology - Web Development<br>Age: 20<br>Short Description: I live in Angeles City, with one kid (orange persian cat).");
+})
+
 var server = app.listen(4000, function() {
     var host = server.address().address
     var port = server.address().port
 
     console.log('Example app listening at http://%s:%s', host, port);
+
 });
